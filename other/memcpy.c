fd4e500aa76c1af16d55c74a219c6b3c9d72318a
Wiffzack <wiffzackius@gmail.com>
2019-01-10 21:47:29 +0100

    Create memcpy.c

---
+++ other/memcpy.c
@@ -0,0 +1,49 @@
+#ifndef fmemcpy
+#define fmemcpy
+#ifndef sseinclude
+#include <smmintrin.h>
+#include <immintrin.h>
+#endif
+//#include <asmlib.h>
+#include <FastMemcpy.h>
+//#include <FastMemcpy_Avx.h>
+
+
+extern void *__I_MPI___intel_avx_rep_memcpy(void *str1, const void *str2, size_t n);
+extern void *__I_MPI__intel_fast_memcpy(void *str1, const void *str2, size_t n);
+//extern void *A_memcpy(void *str1, const void *str2, size_t n);
+//extern void *memcpy(void *str1, const void *str2, size_t n);
+
+//extern void *A_memmove(void *str1, const void *str2, size_t n);
+extern void *__I_MPI__intel_fast_memmove(void *str1, const void *str2, size_t n);
+
+
+void* memcpy(void *dst, const void *src, size_t size)
+{
+  if (size <= 128)
+  {
+    return memcpy_tiny(dst, src, size);
+  }
+  if(size > 128 && size <= 512)
+  {
+    return __I_MPI___intel_avx_rep_memcpy(dst, src, size);
+  }
+        if(size > 512 && size <= 1024)
+        {
+                return __I_MPI__intel_fast_memcpy(dst, src, size);
+        }
+  if (size > 1024 && size <= 8192)
+  {
+    return memcpy_fast(dst, src, size);
+  }
+  if( size > 8192 && size <= 1048576 )
+  {
+    return __I_MPI__intel_fast_memcpy(dst, src, size);
+  }
+        if( size > 1048576 )
+        {
+               return __I_MPI___intel_avx_rep_memcpy(dst, src, size);
+        }
+}
+
+#endif
